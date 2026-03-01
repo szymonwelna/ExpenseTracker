d4287ba0ff394f5124bf485adbe64e1e0e7347b8
szymonwelna <szymonwelna04@gmail.com>
2026-03-01 20:24:31 +0100

    Added shadow to ExpenseCard

--- app/src/main/java/com/example/expensetracker/ui/views/components/ExpenseCard.kt
+++ app/src/main/java/com/example/expensetracker/ui/views/components/ExpenseCard.kt
@@ -9,11 +9,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.unit.dp
 import com.example.expensetracker.data.model.Expense
@@ -21,18 +22,22 @@ import com.example.expensetracker.utils.convertToCurrency
 
 @Composable
 fun ExpenseCard(expense: Expense, onClick: () -> Unit) {
+    val cardColor = MaterialTheme.colorScheme.surfaceContainer
+    val textColor = MaterialTheme.colorScheme.onSurface
+
     Button(
         onClick = { onClick() },
         elevation = ButtonDefaults.buttonElevation(4.dp),
         shape = RoundedCornerShape(16.dp),
         colors = ButtonDefaults.buttonColors(
-            containerColor = Color.LightGray,
-            contentColor = Color.Black
+            containerColor = cardColor,
+            contentColor = textColor
         ),
         modifier = Modifier
             .fillMaxWidth()
             .height(68.dp)
             .padding(horizontal = 12.dp, vertical = 4.dp)
+            .shadow(elevation = 4.dp, shape = RoundedCornerShape(16.dp))
     ) {
         Row(
             modifier = Modifier.fillMaxSize(),
