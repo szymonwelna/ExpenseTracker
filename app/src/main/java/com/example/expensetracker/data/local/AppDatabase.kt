5d5ea423dfe7d531487db9c8ffa49cc08ecb7e08
szymonwelna <szymonwelna04@gmail.com>
2026-03-01 21:27:50 +0100

    Add Database

---
+++ app/src/main/java/com/example/expensetracker/data/local/AppDatabase.kt
@@ -0,0 +1,33 @@
+package com.example.expensetracker.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.expensetracker.data.DateConverter
+import com.example.expensetracker.data.model.Expense
+
+@Database(entities = [Expense::class], version = 1)
+@TypeConverters(DateConverter::class)
+abstract class AppDatabase: RoomDatabase() {
+    
+    abstract fun expenseDao(): ExpenseDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
